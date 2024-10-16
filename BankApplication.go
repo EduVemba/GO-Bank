ce668a1ba1e6a4752d9b4bfeae56562035cfd4f0
EduVemba <eduvemba18@gmail.com>
2024-10-16 17:54:56 +0100

    ADD func addDinheiro function criada para adicionar deinheiro na conta.

--- BankApplication.go
+++ BankApplication.go
@@ -1,12 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 type Tipo = map[int]string
 
 type Conta struct {
 	Tipo           Tipo
 	ValordeEntrada int64
+	Dinheiro       float64
+}
+
+func addDinheiro(conta *Conta) {
+	var NovoDinheiro float64
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Qual é o Valor da Conta que deseja Meter")
+	scanner.Scan()
+	NovoDinheiro, err := strconv.ParseFloat(scanner.Text(), 64)
+	if err != nil {
+		fmt.Println("Valor Errado")
+	}
+	if NovoDinheiro > 800 && NovoDinheiro < 0 {
+		fmt.Println("O Valor está errado")
+	}
+	conta.Dinheiro = NovoDinheiro
+	fmt.Printf("Valor atualizado da conta: %.2f€\n", conta.Dinheiro)
+}
+
+func removeDinheiro(conta *Conta) float64 {
+	var dinheiro float64
+
 }
 
 func abrirConta(conta Conta) Conta {
@@ -17,8 +44,6 @@ func abrirConta(conta Conta) Conta {
 
 	return NovaConta
 }
-
-func AbrirConta() {}
 
 func main() {
 
